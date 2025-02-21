a50516c8fc332a8a38300a2bdcc811779ae8f458
Ahmed Waheed <ahm761171@gmail.com>
2025-02-22 00:32:25 +0200

    Refactor authentication feature: replace LoginView widget, add UserDataModel, and update plugin registrations

---
+++ lib/features/Authentication/presentation/pages/login_view.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Login View',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
-import 'package:e_commerce_app_admin_website_using_flutter_supabase/features/auth/views/login_view.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'core/app_colors.dart';
+import 'features/Authentication/presentation/pages/login_view.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://ckculgffeqpjznbmgfug.supabase.co',
+    // apiKey: 'your
+    // -anon-key',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNrY3VsZ2ZmZXFwanpuYm1nZnVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAxNDk4NzEsImV4cCI6MjA1NTcyNTg3MX0.7Jew1_7KOHyUWfG0Dh8BrILqAbcB9oEo4GEo30XNLI0',
+  );
   runApp(const FlutterEcommerceApp());
 }
 
@@ -11,6 +22,9 @@ class FlutterEcommerceApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: AppColors.kScaffoldColor,
+      ),
       debugShowCheckedModeBanner: false,
       title: 'e-commerce app',
       home: const LoginView(),
